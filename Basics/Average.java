a385791aa21e2ca4e3b7bf2d4dd3e5463ace5ba7
git-amanpandey <ap859151@gmail.com>
2024-08-05 11:30:02 +0530

    Average of number

---
+++ Basics/Average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Average{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int a= nextInt();
+        int b= nextInt();
+        int c= nextInt();
+        float avg= (a+b+c)/3;
+        System.out.println("average :"+ avg);
+    }
+    
+}
